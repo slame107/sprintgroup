331fbe559674350c71b22df4e10742adbfe7835a
JJ <jmung222@uwsp.edu>
2019-04-09 08:57:58 -0500

    surveydata page removed active

--- Sprint2/surveydata.php
+++ Sprint2/surveydata.php
@@ -7,7 +7,7 @@ $page->finalizeTopSection();
 $page->finalizeBottomSection();
 print $page->getTopSection();
 print "<div class='sidebar'>";
-	print "<a class='active' href='index.php' title='Click here to see our home page'>Home Page</a>";
+	print "<a href='index.php' title='Click here to see our home page'>Home Page</a>";
 	print "<a href='survey.php' title='Click here to take our survey'>Survey</a></li>";
 	print "<a href='privacy.php' title='Click here to see our privacy policy'>Privacy policy</a>";
 	print "<a href='search.php' title='Click here to search for an album'>Search</a>";
